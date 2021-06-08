d30b5dfb47f55e3345b3301240e1e4e861d9212d
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-06-08 22:07:24 +0300

    404 pagr

---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+    return (
+        <div>
+            <h3>Portfolio</h3>
+            <h3>Projects I've created</h3>
+        </div>
+    )
+}
---
+++ src/pages/projects/test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function test() {
+    return (
+        <div>
+            <h2>test page</h2>
+        </div>
+    )
+}
