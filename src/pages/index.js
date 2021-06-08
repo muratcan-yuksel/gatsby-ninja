4dd0c7e7372bc8a194fb96f51495b8f32f2381fd
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-06-08 22:13:10 +0300

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from "../components/Navbar"
 
 export default function index() {
     return (
         <div>
+        <Navbar />
             <h3>Design</h3>
             <h4>Develop & Deploy</h4>
             <p>Web developer based in Kuşadası</p>
--- src/pages/projects/test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function test() {
-    return (
-        <div>
-            <h2>test page</h2>
-        </div>
-    )
-}
