454b708c015857ec57dcb40002c1e23768394da3
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-06-08 22:01:55 +0300

    .

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const about = () => {
+    return (
+        <div>
+            <h2>A bout page</h2>
+            <p>Lorem ipsum dolot sit amet</p>
+            <p>Lorem ipsum dolot sit amet</p>
+            <p>Lorem ipsum dolot sit amet</p>
+        </div>
+    )
+}
+
+export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function index() {
+    return (
+        <div>
+            <h3>Design</h3>
+            <h4>Develop & Deploy</h4>
+            <p>Web developer based in Kuşadası</p>
+        </div>
+    )
 }
