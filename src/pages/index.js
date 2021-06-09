8f87c3bd9efcfc74c0cf34bbadefdc9060470505
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-06-09 03:10:04 +0300

    learning about queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 
-export default function index() {
+export default function index({data}) {
+    console.log(data)
+    // lets deconstruct
+    const {title, description} = data.site.siteMetadata
     return (
         <Layout>
         <section  className={styles.header}>
@@ -14,7 +17,22 @@ export default function index() {
             <Link to="/portfolio" className={styles.btn}>My projects</Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{maxWidth: "100%"}}></img>
+       <p>{title} - {description} </p>
         </section>
         </Layout>
     )
 }
+
+// this query is found upstairs as a pros, the "data" I logged gives this siteInfo object
+
+export const query= graphql`
+query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+  
+`
