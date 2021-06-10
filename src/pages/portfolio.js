ec7b5be987dcc95e4a28ce0ff26bd4ca5fe8ce47
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-06-10 03:28:19 +0300

    spent 15 mins on a typo

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,4 +1,4 @@
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from "../components/Layout"
 import * as styles from "../styles/projects.module.css"
@@ -6,11 +6,28 @@ import * as styles from "../styles/projects.module.css"
 
 export default function portfolio({data}) {
     console.log(data)
+    const projects=data.allMarkdownRemark.nodes
+    console.log(data.allMarkdownRemark.nodes[1].frontmatter.title)
+    console.log(projects[1].frontmatter.title)
+
+    
     return (
         <Layout>
         <div className={styles.portfolio}>
               <h3>Portfolio</h3>
             <h3>Projects I've created</h3>
+            <div>
+                {projects.map(project=>(
+                    <Link to={"/projects/" + project.frontmatter.slug} key={project.id} >
+                        <div>
+                            <h3>{project.frontmatter.title}</h3>
+                            <p> {project.frontmatter.stack} </p>
+
+                        </div>
+                    </Link>
+                ))}
+                
+            </div>
         </div>
         </Layout>
     )
